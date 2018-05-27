864f523757546349cc209e7302332dcf0fb85542
Andy Mender <andymenderunix@gmail.com>
2018-05-27 23:06:47 +0200

    Improve 'cp' implementation

--- CopyFile.java
+++ CopyFile.java
@@ -10,10 +10,6 @@ class CopyFile {
         // character placeholder
         int i;
 
-        // file placeholders
-        FileInputStream aFileIn = null;
-        FileOutputStream aFileOut = null;
-
         // check command-line argument number
         if (args.length != 2) {
             System.out.println("Wrong number of arguments.");
@@ -21,23 +17,10 @@ class CopyFile {
             return;
         }
 
-        // sanity-check for input file
-        try {
-            aFileIn = new FileInputStream(args[0]);
-        } catch (FileNotFoundException aExc) {
-            System.out.println("Input file not found. Aborting operation.");
-            return;
-        }
-
-        // sanity-check for output file
-        try {
-            aFileOut = new FileOutputStream(args[1]);
-        } catch (FileNotFoundException aExc) {
-            System.out.println("Target file not created. Aborting operation.");
-            return;
-        }
         // buffer the input file and write to output file
-        try {
+        // use the 'try' context manager
+        try(FileInputStream aFileIn = new FileInputStream(args[0]);
+            FileOutputStream aFileOut = new FileOutputStream(args[1])) {
             do {
                 // possible to do in a single line without buffering
                 // but that removes the extra -1 sanity check
@@ -45,16 +28,7 @@ class CopyFile {
                 if (i != -1) aFileOut.write(i);
             } while (i != -1);
         } catch (IOException aExc) {
-            System.out.println("While copying content a problem has occurred and the program will close.");
-            return;
-        } finally {
-            // can be split to address input and output file individually
-            try {
-                if (aFileIn != null) aFileIn.close();
-                if (aFileOut != null) aFileOut.close();
-            } catch (IOException aExc) {
-                System.out.println("FATAL ERROR: input or output file not closed.");
-            }
+            System.out.println("I/O Error: " + aExc);
         }
     }
 }
