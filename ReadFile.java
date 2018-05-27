1345aa3a071f27f72ce791aa7e1a15d3355c3283
Andy Mender <andymenderunix@gmail.com>
2018-05-27 23:00:51 +0200

    Improve 'cat' implementation

--- ReadFile.java
+++ ReadFile.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 class ReadFile {
     public static void main(String[] args) {
         int i;  // placeholder for read characters
-        FileInputStream aFile = null; // placeholder for the file object
 
         // file name input as command-line argument
         if (args.length != 1) {
@@ -20,24 +19,17 @@ class ReadFile {
         // EOF is marked with -1 (if exceeded, EOFException will be raised);
         // catching FileNotFoundException is not mandatory, because it's a child class
         // of IOException - here it provides extra error reporting
-        try {
-            aFile = new FileInputStream(args[0]);
+        //
+        // this version uses a context manager mimic implemented in JDK 7
+        // which relies on the close() method defined in AutoCloseable and Closeable interfaces
+        // and simplifies the 'catch' clause
+        try(FileInputStream aFile = new FileInputStream(args[0])) {
             do {
                 i = aFile.read();
                 if (i != -1) System.out.print((char) i);
             } while (i != -1);
-        } catch (FileNotFoundException aExc) {
-            System.out.printf("ERROR: File %s not found.\n", args[0]);
         } catch (IOException aExc) {
-            System.out.println("ERROR: Cannot read file.");
-        } finally {
-            try {
-                if (aFile != null) aFile.close();
-            } catch (IOException aExc) {
-                System.out.println("FATAL ERROR: Cannot close file descriptor.");
-            }
+            System.out.println("I/O error: " + aExc);
         }
-        // in Python, the above scenario can be handled using a context manager
-        // which implements closing of the file on exit from the context/scope
     }
 }
