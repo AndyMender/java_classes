36bf45d4746493b28cee5ad66b03c469dd66b6bb
Andy Mender <andymenderunix@gmail.com>
2018-06-10 19:53:08 +0200

    Add 'cp' implementation based on Oracle's tutorials

---
+++ ByteIO.java
@@ -0,0 +1,25 @@
+// File I/O using standard byte streams (Oracle Java Tutorials)
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class ByteIO {
+	public static void main(String[] args) throws IOException {
+		// NOTE: all byte stream classes are derived from InputStream
+		// or OutStream
+		int c;
+		
+		// this requires xanadu.txt to be found here:
+		// https://docs.oracle.com/javase/tutorial/essential/io/streams.html
+		try(FileInputStream inFile = new FileInputStream("xanadu.txt");
+			FileOutputStream outFile = new FileOutputStream("outfile.txt");) {
+			// read file byte by byte and write to another file
+			while ((c = inFile.read()) != -1)
+				outFile.write(c);
+			
+		} catch (IOException aExc) {
+			System.out.println("An exception has occurred: " + aExc);
+			aExc.printStackTrace();
+		}	
+	}
+}
