74a3aeaf0c255f73ef0c10c9eb3f4c378da71c37
Andy Mender <andymenderunix@gmail.com>
2018-06-10 23:03:22 +0200

    Add scanner for numbers

---
+++ ScannerDemo2.java
@@ -0,0 +1,37 @@
+// I/O using scanner objects with locale handling (Oracle Java Tutorials)
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
+
+class ScannerDemo2 {
+	public static void main(String[] args) throws IOException {
+		double sum = 0;
+		
+		// Scanner objects handle also numerical tokens!
+		
+		// below code requires "usnumbers.txt", to be found here:
+		// https://docs.oracle.com/javase/tutorial/essential/io/scanning.html
+		try(Scanner s 
+			= new Scanner(new BufferedReader(new FileReader("usnumbers.txt")));) {
+			// set the US locale to handle the thousands comma notation
+			s.useLocale(Locale.US);
+			// each of the primitive types has an individual method
+			// NOTE: overloading wouldn't work due to the underlying
+			// char-based byte I/O approach
+			while (s.hasNext()) {
+				// read in ONLY tokens of type double
+				if (s.hasNextDouble()) {
+					sum += s.nextDouble();
+				} else {
+					s.next();
+				}
+			}
+		} catch (IOException aExc) {
+			System.out.println("An exception has occurred: " + aExc);
+			aExc.printStackTrace();
+		}
+		System.out.println("The sum is: " + sum);
+	}
+}
